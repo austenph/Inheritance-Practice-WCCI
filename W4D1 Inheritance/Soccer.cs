f3aeb745d7d5f365d241c3af7ce4e89a191d08f4
Austen Perelman-Hall <austenph@gmail.com>
2017-01-23 11:20:23 -0500

    initial commit

---
+++ W4D1 Inheritance/Soccer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4D1_Inheritance
+{
+    class Soccer : Sports
+    {
+
+    }
+}
